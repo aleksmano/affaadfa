16e6df44a3fb2c01e695abae3c22964b5f2cb257
aleksmano <aleksmano@yandex.ru>
2018-05-19 00:20:00 +0300

    new repo

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=hack',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+
+];
