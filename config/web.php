4176de2ed52da68f306eec469a55ce4b55910c3e
aleksmano <aleksmano@yandex.ru>
2018-05-19 20:22:21 +0300

    add profile

--- config/web.php
+++ config/web.php
@@ -12,13 +12,7 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'rules' => [
-      // your rules go here
-            ],
-  // ...
-        ],
+        
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'www',
